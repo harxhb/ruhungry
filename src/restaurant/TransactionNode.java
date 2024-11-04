4aba2dda8fdd72a19b45ef56a014bdef0d467719
Harsh Borkhetaria <harsh.borkhetaria@gmail.com>
2024-11-03 19:25:41 -0500

    first

---
+++ src/restaurant/TransactionNode.java
@@ -0,0 +1,35 @@
+package restaurant;
+
+/**
+ * This file is the "node" in the Transaction linked list
+*/
+
+public class TransactionNode{
+    private TransactionData data; // data portion of node
+    private TransactionNode next; // link to next node in the list
+
+    /*
+     * Default constructor
+     */
+    public TransactionNode (){
+        data = null;
+        next = null;
+    }
+
+    /*
+     * Constructor
+     * @param data object holding information about a transaction
+     * @param next link to the next node in the list
+     */
+    public TransactionNode(TransactionData data, TransactionNode next){
+        this.data = data;
+        this.next = next;
+    }
+
+    // "To Get" and "To Set" Methods
+    public TransactionData getData() { return data;}
+    public TransactionNode getNext() { return next;}
+
+    public void setData(TransactionData dataInput) {data = dataInput;}
+    public void setNext(TransactionNode nextInput) {next = nextInput;}
+}
